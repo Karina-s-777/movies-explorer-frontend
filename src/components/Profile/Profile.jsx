6ae244cf2af460d0d3e78172ac076f5f96ddfe54
Karina Savilova <savilovakarina@yandex.ru>
2023-10-07 11:55:11 +0200

    добавить деактивацию кнопки сохранения в профиле

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,91 +7,96 @@ import useFormValidation from "../../hooks/useFormValidation";
 import CurrentUserContext from '../../contexts/CurrentUserContext'
 
 function Profile({ editUserData, logOut, isError, isSuccess, setIsSuccess, setIsError }) {
-  const currentUser = useContext(CurrentUserContext)
-  // пока что через disabled мы регулирем активность и неактивность кнопки
+  const currentUser = useContext(CurrentUserContext);
   const { values, isValid, handleChange, reset } = useFormValidation();
-
   const [isVisibleSubmit, setIsVisibleSubmit] = useState(false);
+  const [isUserDataChanged, setIsUserDataChanged] = useState(false);
 
   useEffect(() => {
-    reset({ username: currentUser.name, email: currentUser.email })
-  }, [reset, currentUser])
+    reset({ username: currentUser.name, email: currentUser.email });
+    setIsUserDataChanged(false); // Сбросить isUserDataChanged при сбросе значений
+  }, [reset, currentUser]);
 
-  // useEffect(() => {
-  //   setIsError(false)
-  //   setIsSuccess(false);
-  // }, [setIsError, setIsSuccess, values])
-  
+  useEffect(() => {
+    if (values.username !== currentUser.name || values.email !== currentUser.email) {
+      setIsUserDataChanged(true);
+    } else {
+      setIsUserDataChanged(false);
+    }
+  }, [values.username, values.email, currentUser.name, currentUser.email]);
 
   function onSubmit(evt) {
-    evt.preventDefault()
-    editUserData(values.username, values.email)
+    evt.preventDefault();
+    editUserData(values.username, values.email);
     setIsVisibleSubmit(false);
     setIsSuccess(false);
-    setIsError(false)
+    setIsError(false);
   }
 
   function makeButtonVisible() {
     setIsVisibleSubmit(true);
+    setIsUserDataChanged(false);  // Сбрасываем isUserDataChanged, когда кнопка становится видимой
   }
 
   return (
-  <>
-    <Header name="movies"/>
-    <section className="profile">
-    <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
-    <form className="profile__container" onSubmit={onSubmit}>
-      <div className={`profile__container-info ${(isError || isSuccess) && 'profile__container-info_status'}`} >
-      
-        <span className="profile__info-text">Имя</span>
-        <input
-          className="profile__info-text"
-          type="nameUser"
-          id="nameUser"
-          name="nameUser"
-          placeholder="Карина"
-          required
-          minLength="2"
-          value={values.username ? values.username : ""}
-          onChange={handleChange}
-        />
-        {/* <span className={`${!isValid ? "auth__input-error" : "auth__input-error-noactive"}`}>{errors.nameUser}</span> */}
-      </div>
-      <div className="profile__container-info">
-        <span className="profile__info-text profile__info-text-last">E-mail</span>
-        <input
-          className="profile__info-text profile__info-text-last"
-          type="email"
-          id="email"
-          name="email"
-          required
-          placeholder="cat@gmail.com"
-          minLength="2"
-          value={values.email ? values.email : ""}
-          onChange={handleChange}
-        />
-        {/* <span className={`${!isValid ? "auth__input-error" : "auth__input-error-noactive"}`}>{errors.email}</span> */}
-      </div>
-    </form>
-    {isVisibleSubmit ? (
-      <>
-      <button type="submit" className={`${isValid  ? "profile__button-retention" : (!isValid || isError) ? "profile__button-retention_disabled" : "profile__button-retention" }`} onClick={onSubmit} disabled={!isValid}>
-        Сохранить
-      </button>
-      </>
-    ) : (
-   <>
-    <span className={`profile__error-request ${(isError || isSuccess) && 'profile__error-request_status'}`}>{isError ? 'При обновлении профиля произошла ошибка.' : 'Успешно'}</span>
-    <button type="button" className="profile__button-transition" onClick={makeButtonVisible}>
-     Редактировать
-   </button>
-     <Link to="/signin" className="profile__link-exit"  onClick={logOut}>
-     Выйти из аккаунта
-    </Link>
-     </>
-    )}
-  </section>
-  </>
+    <>
+      <Header name="movies"/>
+      <section className="profile">
+        <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
+        <form className="profile__container" onSubmit={onSubmit}>
+          <div className={`profile__container-info ${(isError || isSuccess) && 'profile__container-info_status'}`} >
+            <span className="profile__info-text">Имя</span>
+            <input
+              className="profile__info-text"
+              type="text"
+              id="nameUser"
+              name="username"
+              placeholder="Карина"
+              required
+              minLength="2"
+              value={values.username ? values.username : ""}
+              onChange={(evt) => { handleChange(evt); setIsUserDataChanged(true); }}
+            />
+          </div>
+          <div className="profile__container-info">
+            <span className="profile__info-text profile__info-text-last">E-mail</span>
+            <input
+              className="profile__info-text profile__info-text-last"
+              type="email"
+              id="email"
+              name="email"
+              required
+              placeholder="cat@gmail.com"
+              minLength="2"
+              value={values.email ? values.email : ""}
+              onChange={(evt) => { handleChange(evt); setIsUserDataChanged(true); }}
+            />
+          </div>
+        </form>
+        {isVisibleSubmit ? (
+          <>
+            <button
+              type="submit"
+              className={`profile__button-retention ${(!isValid || !isUserDataChanged || isError) && "profile__button-retention_disabled"}`}
+              onClick={onSubmit}
+              disabled={!isValid || !isUserDataChanged}
+            >
+              Сохранить
+            </button>
+          </>
+        ) : (
+          <>
+            <span className={`profile__error-request ${(isError || isSuccess) && 'profile__error-request_status'}`}>{isError ? 'При обновлении профиля произошла ошибка.' : 'Успешно'}</span>
+            <button type="button" className="profile__button-transition" onClick={makeButtonVisible}>
+              Редактировать
+            </button>
+            <Link to="/signin" className="profile__link-exit"  onClick={logOut}>
+              Выйти из аккаунта
+            </Link>
+          </>
+        )}
+      </section>
+    </>
   );
 }
 
